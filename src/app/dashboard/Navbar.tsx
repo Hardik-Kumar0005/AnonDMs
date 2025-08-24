906b0c4b505c9767e23f175b61e007460ebb51b9
Hardik-Kumar0005 <hardikkumarpro0005@gmail.com>
2025-08-24 12:56:40 +0530

    Add copy link tooltip image and update Navbar layout

--- src/app/dashboard/Navbar.tsx
+++ src/app/dashboard/Navbar.tsx
@@ -6,17 +6,19 @@ import Navigation from "@/utils/Navigation"
 
 function Navbar() {
   return (
-    <div className="sticky top-0 left-0 right-0 z-50 grid grid-cols-3 items-center px-4 py-2">
+    <div className="sticky top-0 left-0 right-0 z-50 grid grid-cols-3 items-center px-2 py-2 bg-transparent ">
               <Button variant="outline" className="justify-self-start mr-auto bg-cyan-400 rounded-2xl">
             <Navigation href="/" label="Home" className="" /> 
 
               </Button>
 
               {/* REPLACE WITH URL COPY BUTTON */}
-            <h1 className="text-center font-semibold bg-cyan-400 rounded-2xl w-fit mx-auto p-2 outline-1">DASH NAV</h1>
+            <h1 className="text-center font-semibold bg-cyan-400 rounded-2xl w-fit mx-auto p-2 outline-1">
+              Copy your anonymous messages link!
+            </h1>
 
 
-              <Button variant="outline" className="justify-self-end bg-cyan-400 rounded-2xl ml-auto">
+              <Button variant="outline" className="justify-self-end bg-cyan-400 rounded-2xl ml-auto hover:bg-red-400">
                 <Navigation href="/logout" label="Logout" className="" />
               </Button>
         </div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Image from 'next/image'
 
 function page() {
   return (
     <>
       <Navbar />
+      <div className='fixed left-1/2 transform -translate-x-1/2'>
+        <Image draggable="false" style={{ 
+            pointerEvents: 'none',
+            userSelect: 'none',
+            MozUserSelect: 'none',
+            WebkitUserSelect: 'none',
+         }}
+          src="/dashArrow.png" alt="Description" width={300} height={300} />
+      </div>
       <div className='w-screen h-screen flex p-4 justify-center items-center'>
         DASHBOARD
       </div>
