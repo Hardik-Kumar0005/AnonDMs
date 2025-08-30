27c02de318889f44e1815423d816db1ef5df00de
Hardik-Kumar0005 <hardikkumarpro0005@gmail.com>
2025-08-30 10:55:01 +0530

    Implement 3D Cat model in the application Add a new Model component as a wrapper

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button'
 function Navbar() {
     return (
         <>
-            <div className="fixed top-0 left-0 right-0 z-50 grid grid-cols-3 justify-center place-items-center px-2 border mt-2 mx-2 py-2 bg-transparent backdrop-blur-3xl rounded-3xl shadow">
+            <div className="fixed top-0 left-0 right-0 z-50 grid grid-cols-3 justify-center place-items-center px-2 border mt-2 py-2 mx-2 bg-transparent rounded-3xl shadow backdrop-blur-2xl duration-500">
                 <div className="flex flex-col md:block justify-self-start items-start gap-2">
                     <Button className="bg-cyan-400 rounded-2xl px-3 py-1 text-black hover:bg-amber-400 hover:font-bold duration-300">
                     <Navigation href="/" label="Home" className="" />
---
+++ src/assets/Cat.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+function Cat() {
+
+  const gltf = useLoader(GLTFLoader, "/cat/scene.gltf");
+  return (
+    <primitive object={gltf.scene} scale={6.0} position={[-1, -29, -5]} />
+  );
+};
+
+export default Cat;
